352a39e4611590f467e1ae4abf5fd7d785749ff8
barimehdi77 <mbari@student.1337.ma>
2024-01-23 23:56:05 +0100

    feat(WIP): this not working

--- p1/scripts/k3s_installer_as_master.sh
+++ p1/scripts/k3s_installer_as_master.sh
@@ -1,3 +1,12 @@
 
+echo "192.168.56.110   mbariS" >> /etc/hosts
+
+# to disable the firewall and allow conection to the machine
+sudo systemctl disable firewalld --now
+
 # install k3s without need of `sudo`
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644 --node-ip "192.168.56.110"
+
+sudo cat /var/lib/rancher/k3s/server/token > /tmp/token
+
+ls -la /tmp/
