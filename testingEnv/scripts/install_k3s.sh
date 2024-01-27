d96fbb4979b02ea8a196f0c010b1718640178d67
barimehdi77 <mbari@student.1337.ma>
2024-01-28 00:18:25 +0100

    learnig k8s

---
+++ testingEnv/scripts/install_k3s.sh
@@ -0,0 +1,8 @@
+# to disable the firewall and allow conection to the machine
+sudo systemctl disable firewalld --now
+
+# install k3s without need of `sudo`
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644
+export PATH=$PATH:/usr/local/bin
+# create the pods in the ../pods/fifo-pod.yml
+k3s kubectl apply -f /vagrant/pods/fifo-pod.yml
