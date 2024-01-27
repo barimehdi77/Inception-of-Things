f5d3d71b13d772ffab56476b26ef944906edf6b8
barimehdi77 <mbari@student.1337.ma>
2024-01-27 19:04:20 +0100

    first part is done

--- p1/scripts/k3s_installer_as_agent.sh
+++ p1/scripts/k3s_installer_as_agent.sh
@@ -1,4 +1,3 @@
-echo "192.168.56.110   mbariS" >> /etc/hosts
-ls -la /tmp
-export TOKEN=`cat /tmp/token`
+echo "192.168.56.110 mbariS" >> /etc/hosts
+export TOKEN=`cat /vagrant/token`
 curl -sfL https://get.k3s.io | K3S_URL="https://mbariS:6443" K3S_TOKEN=$TOKEN sh -s - --node-ip "192.168.56.111"
--- p1/scripts/k3s_installer_as_master.sh
+++ p1/scripts/k3s_installer_as_master.sh
@@ -7,6 +7,4 @@ sudo systemctl disable firewalld --now
 # install k3s without need of `sudo`
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644 --node-ip "192.168.56.110"
 
-sudo cat /var/lib/rancher/k3s/server/token > /tmp/token
-
-ls -la /tmp/
+sudo cat /var/lib/rancher/k3s/server/token > /vagrant/token
