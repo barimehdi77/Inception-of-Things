95b8870895e375c441481f3b1106dffbace70167
barimehdi77 <mbari@student.1337.ma>
2024-01-28 15:42:16 +0100

    feat: test some stuff

--- testingEnv/scripts/install_k3s.sh
+++ testingEnv/scripts/install_k3s.sh
@@ -4,5 +4,7 @@ sudo systemctl disable firewalld --now
 # install k3s without need of `sudo`
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644
 export PATH=$PATH:/usr/local/bin
+pwd
+ls -la
 # create the pods in the ../pods/fifo-pod.yml
 k3s kubectl apply -f /vagrant/pods/fifo-pod.yml
