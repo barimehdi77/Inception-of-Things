4b6c72dbc875547de4d2160ffe0d92f2750f2d53
Bari Mehdi77 <barimehdi777@gmail.com>
2024-03-02 00:01:46 +0100

    feat: add all apps and there services with ingress

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -3,12 +3,15 @@ sudo systemctl disable firewalld --now
 
 # install k3s without need of `sudo`
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644 --node-ip "190.168.42.110"
-# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644
 
-# export PATH=$PATH:/usr/local/bin
-# pwd
-# ls -la
-# # Create a default sa
-# /usr/local/bin/kubectl create sa default
-# # create the pods in the ../pods/fifo-pod.yml
+
 /usr/local/bin/kubectl apply -f /vagrant/confs/app-one.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-two.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-three.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-one-service.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-two-service.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-three-service.yaml
+
+
+
+/usr/local/bin/kubectl apply -f /vagrant/confs/ingress.yaml
