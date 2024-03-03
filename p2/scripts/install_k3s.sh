8f0d2cfba0fdc504a1009fa0d28e7c51a7fc805f
barimehdi77 <mbari@student.1337.ma>
2024-03-03 18:38:07 +0100

    feat: part2 is done

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -2,7 +2,8 @@
 sudo systemctl disable firewalld --now
 
 # install k3s without need of `sudo`
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644 --node-ip "190.168.42.110"
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode 644 --node-ip "190.168.56.110"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip 192.168.56.110"  sh -s -;
 
 
 /usr/local/bin/kubectl apply -f /vagrant/confs/app-one.yaml
