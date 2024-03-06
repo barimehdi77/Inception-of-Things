662f966f03894c374113279f5d17eb9ea93bc253
Bari Mehdi77 <barimehdi777@gmail.com>
2024-03-06 19:19:19 +0100

    feat: help

---
+++ p3/scripts/init-project.sh
@@ -0,0 +1,6 @@
+# script
+  # create k3d cluster with loadbalancer
+  # install argo cd
+  # install argo cd command to get password and print it
+  # apply the yaml file to create application in argo cd
+
